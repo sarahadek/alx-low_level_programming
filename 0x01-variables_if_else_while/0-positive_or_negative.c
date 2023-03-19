4b40acab69105efc6925d44e650c68b304f2c9a5
sarahadek <tossie77c@gmail.com>
2023-03-19 04:18:03 -0700

    the value  of n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* determine the value of n  , Exit (sucess)*/
+
+/**
+ * main - Determines if  n is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
 int main (void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if(n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n == 0)
+	else if(n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
@@ -19,5 +25,5 @@ int main (void)
 	{
 		printf("%d is negative\n", n);
 	}
-	return (0);
+	return(0);
 }
