0d3d57c36076d210e08f5dd4a9c9c0677c7e03f6
sarahadek <tossie77c@gmail.com>
2023-04-22 06:36:49 -0700

    allocate memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,41 +2,45 @@
 #include "main.h"
 
 /**
- * memset - fills memory with a constant byte
+ * *_memset - fills memory with a constant byte
  * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
  *
  * Return: pointer to the memory area s
  */
-void *memset(void *s, int b, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	char *p = s;
-	while (n--)
-		*p++ = (unsigned char)b;
-	return s;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
 
 /**
- * calloc - allocates memory for an array
+ * *_calloc - allocates memory for an array
  * @nmemb: number of elements in the array
  * @size: size of each element
  *
  * Return: pointer to allocated memory
  */
-void *calloc(size_t nmemb, size_t size)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
 
 	if (nmemb == 0 || size == 0)
-		return NULL;
+		return (NULL);
 
 	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
-		return NULL;
+		return (NULL);
 
-	memset(ptr, 0, nmemb * size);
+	_memset(ptr, 0, nmemb * size);
 
-	return ptr;
+	return (ptr);
 }
