09f595d8d7f7c04ad1391bf2f80fb5cee5022d36
sarahadek <tossie77c@gmail.com>
2023-05-01 17:12:40 -0700

    sum all parameter

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef VARIADIC_H
-#define VARIADIC_H
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
